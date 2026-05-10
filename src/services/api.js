63901f8102952fb93467101d296b0ff1c565b3a9
Utsab Pandit <utshav2060@gmail.com>
2026-05-10 09:37:51 +0545

    Fixed api gateways

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:8000/api/";
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL || "http://127.0.0.1:8000";
 
 const API = axios.create({
   baseURL: BACKEND_URL,
